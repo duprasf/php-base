74d86fce7f8159e3453eb1b592625e7cadbb9bfd
pgohard <patrick.gohard@canada.ca>
2023-11-24 15:04:59 -0500

    Update go-php.sh
    Fixed comments using wrong syntax, fixed syntax on additional if condition

--- go-php.sh
+++ go-php.sh
@@ -5,20 +5,20 @@ if [ ! -z "$PHP_DEV_ENV" ] && [ "${PHP_DEV_ENV}" == "1" ]; then
     PHP_OPCACHE_VALIDATE_TIMESTAMPS=1
 fi
 
-// backward compatibility
-if [ -z "$PHP_MEMORY_LIMIT" ] && [! -z "$MEMORY_LIMIT" ]; then
+# backward compatibility
+if [ -z "$PHP_MEMORY_LIMIT" ] && [ ! -z "$MEMORY_LIMIT" ]; then
     PHP_MEMORY_LIMIT=$MEMORY_LIMIT
 fi
-if [ -z "$PHP_UPLOAD_SIZE" ] && [! -z "$UPLOAD_SIZE" ]; then
+if [ -z "$PHP_UPLOAD_SIZE" ] && [ ! -z "$UPLOAD_SIZE" ]; then
     PHP_UPLOAD_SIZE=$UPLOAD_SIZE
 fi
-if [ -z "$PHP_POST_MAX_SIZE" ] && [! -z "$POST_MAX_SIZE" ]; then
+if [ -z "$PHP_POST_MAX_SIZE" ] && [ ! -z "$POST_MAX_SIZE" ]; then
     PHP_POST_MAX_SIZE=$POST_MAX_SIZE
 else
     PHP_POST_MAX_SIZE=$PHP_UPLOAD_SIZE
 fi
 
-// call the go-application if provided by the app
+# call the go-application if provided by the app
 if [[ -f "/go-application.sh" ]]; then
     source /go-application.sh
 fi
